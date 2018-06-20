3461128c934d4b9887c34dc87acaaae92edecc00
amaslakov <atom2193@yandex.ru>
2018-06-20 22:49:11 +0300

    ES-2. Add first controller and view.

---
+++ AM.EmergencyService.App/AM.EmergencyService.App.Web/Controllers/RequestController.cs
@@ -0,0 +1,28 @@
+﻿using AM.EmergencyService.App.Web.Providers;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AM.EmergencyService.App.Web.Controllers
+{
+    public class RequestController : Controller
+    {
+        private readonly IDataProvider _dataProvider;
+
+        public RequestController(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        // GET: Request
+        public ActionResult Index()
+        {
+            var requests = _dataProvider.GetData();
+
+            return View(requests);
+        }
+    }
+}
---
+++ AM.EmergencyService.App/AM.EmergencyService.App.Web/Models/RequestModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AM.EmergencyService.App.Web.Models
+{
+    public class RequestModel : IModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Номер вызова")]
+        public int RequestNumber { get; set; }
+        [Display(Name = "Адрес вызова")]
+        public string RequestAddress { get; set; }
+        [Display(Name = "Повод вызова")]
+        public string RequestReason { get; set; }
+        [Display(Name = "Время вызова")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
+        public string RequestDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Время передачи бригаде МЧС")]
+        public string BrigadeCallDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Время прибытия")]
+        public string BrigadeArrivalDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Время окончания вызова")]
+        public string BrigadeEndDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Время возвращения в часть")]
+        public string BrigadeReturnDate { get; set; }
+        [Display(Name = "Категория вызова")]
+        public string RequestCategory { get; set; }
+    }
+}
